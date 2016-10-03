bdaf3d26fdb6223010e7f26b2175b54cb905a969
Thomas Laegler <thomas.laegler@googlemail.com>
2016-10-03 22:48:12 +0200

    Cleaned Files

--- rest-api/src/gen/java/de/tk/tkapp/rest/api/invoker/RestApplication.java
+++
@@ -1,9 +0,0 @@
-package de.tk.tkapp.rest.api.invoker;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/")
-public class RestApplication extends Application {
-
-}
